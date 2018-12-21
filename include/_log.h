a2aa1436f052459f16de94d58a66907bde76510a
masoud <masoud.bolhassani@gmail.com>
2018-12-21 22:29:03 +0330

    log changed to support multi file

--- include/_log.h
+++ include/_log.h
@@ -5,7 +5,7 @@
 #include <boost/log/sources/global_logger_storage.hpp>
 
 // the logs are also written to LOGFILE
-#define LOGFILE "logfile.log"
+#define LOGFILE "log/file_%5N.log" //"logfile.log"
 
 // just log messages with severity >= SEVERITY_THRESHOLD are written
 #define SEVERITY_THRESHOLD logging::trivial::trace
