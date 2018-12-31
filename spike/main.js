2b9f790f50404ac1747aaec0a0f318d07c61fa13
masoud bolhassani <masoud.bolhassani@gmail.com>
2018-12-31 19:09:37 +0330

    main.js

--- spike/main.js
+++ spike/main.js
@@ -8,11 +8,12 @@ $( document ).ready(function() {
     $('.ajax_form').on('submit', function (e) {
         e.preventDefault();
         $.ajax({
-            url: "http://198.143.183.251:9080/login",
+            url: "http://198.143.183.251:9080/users/2",
             //url: "http://127.0.0.1:9080/login",
-            type: "post",
+            type: "get",
             headers: {
-                'Content-Type':'application/json'
+                'Content-Type':'application/json',
+                'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NDY1Mjg4NzgsImlhdCI6MTUxMzg2MjM3MSwiaWQiOiJtYXNvdWQuYkBnbWFpbC5jb20iLCJpc3MiOiJtYXNvdWQuYkBnbWFpbC5jb20iLCJzdWIiOiJ0ZXN0IiwidXNlciI6ImFkbWluIn0.skhXJIcWsxpzStMMR_wslYZzUJGSsngHBWhoZZqwOm0'
             },
             dataType: 'json',
             data:JSON.stringify({"email":"masoud@gmail.com","password":"1234","details":""}) ,
