9bc509ddb4ae541f1540a0c60a28aeb9661f0c9c
masoud bolhassani <masoud.bolhassani@gmail.com>
2019-06-02 14:16:43 +0430

    path

--- src/models/avatar_model.cc
+++ src/models/avatar_model.cc
@@ -41,8 +41,8 @@ std::string AvatarModel::GetAvatar(std::string & user_id){
 std::string AvatarModel::AddAvatar(const std::string & user_id, const std::string & filename, const std::string & data,
       size_t offset, size_t length){
 
-  std::string path= "/home/angru/angru/avatars/";
-  //std::string path= "/home/masoud/Projects/angru/avatars/";
+  //std::string path= "/home/angru/angru/avatars/";
+  std::string path= "/home/masoud/Projects/angru/avatars/";
   DIR* dirp = opendir(path.c_str());
   struct dirent * dp;
   while ((dp = readdir(dirp)) != NULL) {
