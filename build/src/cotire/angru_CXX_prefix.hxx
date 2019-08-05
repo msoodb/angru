e4470f51ce53be9552763993884ed5628b890cee
masoud bolhassani <masoud.bolhassani@gmail.com>
2019-08-06 01:58:55 +0430

    .gitignore fix

--- build/src/cotire/angru_CXX_prefix.hxx
+++
@@ -1,5 +0,0 @@
-/* cotire.cmake 1.8.0 generated file */
-/* /home/masoud/Projects/angru/build/src/cotire/angru_CXX_prefix.hxx */
-#ifdef __cplusplus
-#include "/home/masoud/Projects/angru/build/src/cotire/angru_CXX_prefix.cxx"
-#endif
